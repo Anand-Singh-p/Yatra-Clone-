eafeaec6944db4d529de9e86a46f2a91051c21e6
Anand Singh P <kumarsinghanand821@gmail.com>
2024-01-13 13:41:55 +0530

    add database coonection

--- phptable.php
+++ phptable.php
@@ -3,7 +3,7 @@ include("db_connection.php")
 
 //Create admin table
 $sql = "CREATE TABLE IF NOT EXISTS Admin(
-    AdminID INT AUTO_INCREMENT PRIMARAY KEY,
+    AdminID INT AUTO_INCREMENT PRIMARY KEY,
     Username VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL
